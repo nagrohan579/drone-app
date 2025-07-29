27b89d5b1e2808de544a86449d96fd6a32b6fafe
Rohan Nag <nagrohan579@gmail.com>
2025-07-29 07:27:02 +0000

    Added the base repo in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/drone-app/', // Set the base path for GitHub Pages
 })
